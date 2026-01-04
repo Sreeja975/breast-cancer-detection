e688a906dafed9f9db36a82ef8c911f7d10eadfe
Sreeja975 <71665339+Sreeja975@users.noreply.github.com>
2026-01-04 09:45:10 +0530

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -2,18 +2,21 @@ import torch
 from torchvision import transforms
 from PIL import Image
 import os
+from model import BreastCancerCNN
 
-# Load trained model
-def load_model(model_path="breast_cancer_cnn.pth"):
+
+def load_model(model_path):
     if not os.path.exists(model_path):
-        raise FileNotFoundError(f"Model file '{model_path}' not found")
+        return None
 
-    model = torch.load(model_path, map_location=torch.device("cpu"))
+    model = BreastCancerCNN()
+    model.load_state_dict(
+        torch.load(model_path, map_location="cpu")
+    )
     model.eval()
     return model
 
 
-# Preprocess uploaded image
 def preprocess_image(image: Image.Image):
     transform = transforms.Compose([
         transforms.Resize((224, 224)),
@@ -23,6 +26,4 @@ def preprocess_image(image: Image.Image):
             std=[0.229, 0.224, 0.225]
         )
     ])
-
-    image_tensor = transform(image).unsqueeze(0)  # Add batch dimension
-    return image_tensor
+    return transform(image).unsqueeze(0)
